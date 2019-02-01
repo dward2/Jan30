fcc9fb7f806fda8363b24b32b71ac7c605dafa31
Dr David Ward <daw74@duke.edu>
2019-02-01 09:51:50 -0500

    Fixed PEP-8 issues in python files

--- temp_conversion_module.py
+++ temp_conversion_module.py
@@ -3,7 +3,7 @@ def convert_c_to_f(temp_C):
     temp_F = temp_C * 1.8 + 32
     return temp_F
 
-    
+
 def main_code():
     temp_C_str = input("Enter temperature in degC: ")
     temp_C = float(temp_C_str)
@@ -11,9 +11,8 @@ def main_code():
     print("The temperature is {} degF.".format(temp_F))
 
 
-    
 def fever_detection(temp_list):
-        
+
     fever_threshold = 100.5
     max_temp = 0
     is_fever = False
@@ -28,8 +27,6 @@ def fever_detection(temp_list):
             is_fever = True
     return max_temp, is_fever
 
-    
+
 if __name__ == "__main__":
     main_code()
-    
-
--- test_temp_conversion_module.py
+++ test_temp_conversion_module.py
@@ -1,39 +1,43 @@
 from temp_conversion_module import convert_c_to_f
 from temp_conversion_module import fever_detection
 
+
 def test_convert_c_to_f():
-    
+
     answer = convert_c_to_f(20.0)
     expected = 68.0
-    
+
     assert answer == expected
-    
+
+
 def test_2():
     answer = convert_c_to_f(-40)
     expected = -40
-    
+
     assert answer == expected
+
 
 def test_3():
     answer = convert_c_to_f(50)
     expected = 122
-    
+
     assert answer == expected
-    
+
+
 def test_fever_detection():
     temp_list = [93.0, 98.0, 100.0, 105.0, 101.0]
     max_temp, is_fever = fever_detection(temp_list)
     expected_max = 105.0
     is_fever = True
-    
+
     assert max_temp == expected_max
-    
+
+
 def test_fever_detection_bad():
     temp_list = [93.0, "98.0", 100.0, 105.0, 101.0]
     max_temp, is_fever = fever_detection(temp_list)
     expected_max = 105.0
     expected_fever = True
-    
+
     assert max_temp == expected_max
     assert is_fever == expected_fever
-    
